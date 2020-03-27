25a6548c59b0bb773201df37795dbbdce4fdd696
SashaVasal <34645577+SashaVasal@users.noreply.github.com>
2020-03-27 11:27:41 +1000

    q

---
+++ Form1.Designer.cs
@@ -0,0 +1,253 @@
+﻿namespace Graph
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+        
+        
+            #region Код, автоматически созданный конструктором форм Windows
+
+            /// <summary>
+            /// Требуемый метод для поддержки конструктора — не изменяйте 
+            /// содержимое этого метода с помощью редактора кода.
+            /// </summary>
+            private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.Line = new System.Windows.Forms.Button();
+            this.BrokenLine = new System.Windows.Forms.Button();
+            this.Pen = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.ChangeColor = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.менюToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.сохранитьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.выйтиToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.canvas = new System.Windows.Forms.PictureBox();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.canvas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.button5);
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.Line);
+            this.panel1.Controls.Add(this.BrokenLine);
+            this.panel1.Controls.Add(this.Pen);
+            this.panel1.Location = new System.Drawing.Point(12, 44);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(258, 216);
+            this.panel1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(123, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(15, 165);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 0;
+            this.button5.Text = "button1";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(15, 127);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 0;
+            this.button4.Text = "button1";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // Line
+            // 
+            this.Line.Location = new System.Drawing.Point(15, 98);
+            this.Line.Name = "Line";
+            this.Line.Size = new System.Drawing.Size(75, 23);
+            this.Line.TabIndex = 0;
+            this.Line.Text = "Line";
+            this.Line.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.Line.UseVisualStyleBackColor = true;
+            this.Line.Click += new System.EventHandler(this.LineClick);
+            // 
+            // BrokenLine
+            // 
+            this.BrokenLine.Location = new System.Drawing.Point(15, 54);
+            this.BrokenLine.Name = "BrokenLine";
+            this.BrokenLine.Size = new System.Drawing.Size(75, 23);
+            this.BrokenLine.TabIndex = 0;
+            this.BrokenLine.Text = "broken line";
+            this.BrokenLine.UseVisualStyleBackColor = true;
+            this.BrokenLine.Click += new System.EventHandler(this.BrokenClick);
+            // 
+            // Pen
+            // 
+            this.Pen.Location = new System.Drawing.Point(15, 14);
+            this.Pen.Name = "Pen";
+            this.Pen.Size = new System.Drawing.Size(75, 23);
+            this.Pen.TabIndex = 0;
+            this.Pen.Text = "pen";
+            this.Pen.UseVisualStyleBackColor = true;
+            this.Pen.Click += new System.EventHandler(this.PenClick);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.ChangeColor);
+            this.panel2.Controls.Add(this.listView1);
+            this.panel2.Location = new System.Drawing.Point(12, 266);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(258, 216);
+            this.panel2.TabIndex = 2;
+            // 
+            // ChangeColor
+            // 
+            this.ChangeColor.Location = new System.Drawing.Point(25, 28);
+            this.ChangeColor.Name = "ChangeColor";
+            this.ChangeColor.Size = new System.Drawing.Size(75, 23);
+            this.ChangeColor.TabIndex = 3;
+            this.ChangeColor.Text = "Color";
+            this.ChangeColor.UseVisualStyleBackColor = true;
+            this.ChangeColor.Click += new System.EventHandler(this.ChangeColor_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(3, 3);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(252, 210);
+            this.listView1.TabIndex = 2;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.менюToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1154, 24);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // менюToolStripMenuItem
+            // 
+            this.менюToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.сохранитьToolStripMenuItem,
+            this.выйтиToolStripMenuItem});
+            this.менюToolStripMenuItem.Name = "менюToolStripMenuItem";
+            this.менюToolStripMenuItem.Size = new System.Drawing.Size(53, 20);
+            this.менюToolStripMenuItem.Text = "Меню";
+            // 
+            // сохранитьToolStripMenuItem
+            // 
+            this.сохранитьToolStripMenuItem.Name = "сохранитьToolStripMenuItem";
+            this.сохранитьToolStripMenuItem.Size = new System.Drawing.Size(133, 22);
+            this.сохранитьToolStripMenuItem.Text = "Сохранить";
+            // 
+            // выйтиToolStripMenuItem
+            // 
+            this.выйтиToolStripMenuItem.Name = "выйтиToolStripMenuItem";
+            this.выйтиToolStripMenuItem.Size = new System.Drawing.Size(133, 22);
+            this.выйтиToolStripMenuItem.Text = "Выйти";
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // canvas
+            // 
+            this.canvas.BackColor = System.Drawing.SystemColors.Control;
+            this.canvas.Location = new System.Drawing.Point(303, 44);
+            this.canvas.Name = "canvas";
+            this.canvas.Size = new System.Drawing.Size(839, 526);
+            this.canvas.TabIndex = 4;
+            this.canvas.TabStop = false;
+            this.canvas.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Canvas_MouseDown);
+            this.canvas.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Canvas_MouseMove);
+            // 
+            // colorDialog1
+            // 
+            this.colorDialog1.Color = System.Drawing.Color.Gray;
+            this.colorDialog1.FullOpen = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(1154, 582);
+            this.Controls.Add(this.canvas);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.menuStrip1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.canvas)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem менюToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem сохранитьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem выйтиToolStripMenuItem;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.PictureBox canvas;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button Line;
+        private System.Windows.Forms.Button BrokenLine;
+        private System.Windows.Forms.Button Pen;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Button ChangeColor;
+        private System.Windows.Forms.ListView listView1;
+        //private System.Windows.Forms.mous
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Graph
+{
+    public partial class Form1 : Form
+    {
+        int count = 1;
+        
+        Bitmap bit;
+        float x1 = 0, y1 = 0;
+        int q = 1;
+        Pen pen = new Pen(Color.Black);
+        public Form1()
+        {
+            bit = new Bitmap(1000, 1000);
+            x1 = y1 = 0;
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+       
+
+        private void Canvas_MouseMove(object sender, MouseEventArgs e)
+        {
+            if(count == 1)
+            {
+                
+                Graphics g;
+                g = Graphics.FromImage(bit);
+                if (e.Button == MouseButtons.Left)
+                {
+                    g.DrawLine(pen, x1, y1, e.X, e.Y);
+                    canvas.Image = bit;
+                }
+                x1 = e.X;
+                y1 = e.Y;
+            }
+            
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public void Canvas_Mouse_Move(object sender, MouseEventArgs e)
+        {
+            
+        }
+        public void PenClick(object sender, EventArgs e)
+        {
+            count = 1;
+            q = 1;
+        }
+
+        private void Canvas_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (count == 2)
+            {
+                if(q == -1)
+                {
+                    
+                    Graphics g;
+                    g = Graphics.FromImage(bit);
+
+                    if (e.Button == MouseButtons.Left)
+                    {
+
+                        g.DrawLine(pen, x1, y1, e.X, e.Y);
+                        canvas.Image = bit;
+                        x1 = e.X;
+                        y1 = e.Y;
+                    }
+                }
+                else
+                {
+                    x1 = e.X;
+                    y1 = e.Y;
+                    q *= (-1);
+                }
+            }
+            
+
+            if (count == 3)
+            {
+                if (q == -1)
+                {                  
+                    Graphics g;
+                    g = Graphics.FromImage(bit);
+                    if (e.Button == MouseButtons.Left)
+                    {
+                        g.DrawLine(pen, x1, y1, e.X, e.Y);
+                        canvas.Image = bit;
+                    }
+                    q *= (-1);
+                }
+                else
+                {
+                    x1 = e.X;
+                    y1 = e.Y;
+                    q *= (-1);
+                }
+            }            
+        }
+     
+        public void BrokenClick(object sender, EventArgs e)
+        {
+            count = 2;
+            q = 1;
+        }
+
+        private void ChangeColor_Click(object sender, EventArgs e)
+        {
+            
+            if (colorDialog1.ShowDialog() == DialogResult.Cancel)
+                 return;
+  
+            pen = new Pen(colorDialog1.Color);
+
+        }
+
+        private void LineClick(object sender, EventArgs e)
+        {
+            count = 3;
+            q = 1;
+        }
+    }
+}
